c21c4f68d7340a188b2ee7c1cbd6df1b3b5531e8
JuanSantos64 <juansanntos17@gmail.com>
2024-01-18 12:01:24 -0300

    Projeto Finalizado

---
+++ MainPage.xaml.cs
@@ -0,0 +1,126 @@
+﻿using AppJogoForca.Libraries.Text;
+using AppJogoForca.Models;
+using AppJogoForca.Repositories;
+using System.Text;
+
+namespace AppJogoForca
+{
+    public partial class MainPage : ContentPage
+    {
+        private Word _word;
+        private int _errors;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ResetScreen();
+            
+        }
+
+        private async void OnButtonClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.IsEnabled = false;
+
+            String letter = button.Text;
+
+            var positions = _word.Text.GetPositions(letter);
+
+            if (positions.Count == 0)
+            {
+                ErrorHandler(button);
+                await IsGameOver();
+                return;
+            }
+
+            ReplaceLetter(letter, positions);
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Sucess"] as Style;
+
+            HasWinner();
+
+        }
+        private void OnButtonClickedResetGame(object sender, EventArgs e)
+        {
+            ResetScreen();
+
+        }
+        #region Handler Sucess
+        private async Task HasWinner()
+        {
+            if (!lblText.Text.Contains("_"))
+            {
+                await DisplayAlert("Ganhou!", "Você ganhou o Jogo", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+
+
+
+        private void ReplaceLetter(string letter, List<int> positions)
+        {
+            foreach (int position in positions)
+            {
+                lblText.Text = lblText.Text.Remove(position, 1).Insert(position, letter);
+
+            }
+        }
+        #endregion
+        #region Handler Failed
+        private void ErrorHandler(Button button)
+        {
+            _errors++;
+            ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Failed"] as Style;
+        }
+
+        private async Task IsGameOver()
+        {
+            if (_errors == 6)
+            {
+                await DisplayAlert("Perdeu!", "Você foi enforcado", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+        #endregion
+        #region Reset Screen - Back Screen to Initial State
+        private void ResetScreen()
+        {
+            ResetErrors();
+            ResetVirtualKeyboard();
+            GenerateWord();
+            
+        }
+        private void GenerateWord()
+        {
+            var repository = new WordRepositories();
+            _word = repository.GetRandomWord();
+
+            lblTips.Text = _word.Tips;
+            lblText.Text = new string('_', _word.Text.Length);
+        }
+        private void ResetErrors()
+        {
+            _errors = 0;
+            ImgMain.Source = ImageSource.FromFile("forca1.png");
+        }
+
+        private void ResetVirtualKeyboard()
+        {
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[0]);
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[1]);
+            ResetVirtualLines((HorizontalStackLayout)KeyboardContainer.Children[2]);
+            
+        }
+
+        private void ResetVirtualLines(HorizontalStackLayout horizontal)
+        {
+            foreach(Button button in horizontal.Children)
+            {
+                button.IsEnabled = true;
+                button.Style = null;
+            }
+        }
+        #endregion
+    }
+
+}
---
+++ Repositories/WordRepositories.cs
@@ -0,0 +1,30 @@
+﻿using AppJogoForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoForca.Repositories
+{
+    public class WordRepositories
+    {
+        private List<Word> _worlds;
+        public WordRepositories()
+        {
+            _worlds = new List<Word>();
+            _worlds.Add(new Word("Nome", "Maria".ToUpper()));
+            _worlds.Add(new Word("Vegetal", "Cenoura".ToUpper()));
+            _worlds.Add(new Word("Fruta", "Abacate".ToUpper()));
+            _worlds.Add(new Word("Tempero", "Baiano".ToUpper()));
+            _worlds.Add(new Word("Tempero", "Nordestino".ToUpper()));
+        
+        }
+        public Word GetRandomWord()
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, _worlds.Count);
+            return _worlds[number];
+        }
+    }
+}
